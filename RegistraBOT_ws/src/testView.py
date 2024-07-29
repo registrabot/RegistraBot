332b9613848c96ead61e19c039fda02cf7d14e7d
JM95 <jose.munoz@utec.edu>
2024-07-29 17:17:44 -0400

    new version 240729

--- RegistraBOT_ws/src/testView.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from viewProduct_weightsensor import VProducto
-
-
-if __name__ == "__main__":
-    app = QApplication([])
-    ventana = VProducto()
-    ventana.show()
-    app.exec_()
